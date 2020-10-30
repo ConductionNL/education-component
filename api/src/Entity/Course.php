9825d0c62b6309a233d6f2e3407262a28014a6f4
Robert Zondervan <Robert@conduction.nl>
2020-10-30 11:59:53 +0100

    Filter on additionalTypes

--- api/src/Entity/Course.php
+++ api/src/Entity/Course.php
@@ -2,7 +2,9 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Core\Annotation\ApiFilter;
 use ApiPlatform\Core\Annotation\ApiResource;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter;
 use App\Repository\CourseRepository;
 use DateTime;
 use Doctrine\Common\Collections\ArrayCollection;
@@ -23,6 +25,8 @@ use Symfony\Component\Validator\Constraints as Assert;
  *     denormalizationContext={"groups"={"write"}, "enable_max_depth"=true}
  * )
  * @ORM\Entity(repositoryClass=CourseRepository::class)
+ *
+ * @ApiFilter(SearchFilter::class, properties={"additionalType": "iexact"})
  */
 class Course
 {
