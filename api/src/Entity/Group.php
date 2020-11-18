38c137f1eaa9a25c9370c13a443ebb0721a3e600
Robert Zondervan <Robert@conduction.nl>
2020-11-18 14:00:43 +0100

    Fix

--- api/src/Entity/Group.php
+++ api/src/Entity/Group.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use ApiPlatform\Core\Annotation\ApiResource;
+use App\Repository\GroupRepository;
 use DateTime;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
